e4f6d6e7b833b8f13988da78e6d3bc0bcafe1958
sophiacofone <sophiacofone@gmail.com>
2023-04-08 19:12:54 -0400

    Update tuits.js

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,10 +1,19 @@
 export default [
     {
-        "_id": "123", "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.jpeg",
+        "_id": "123", 
+        "topic": "Space", 
+        "username": "SpaceX",
+        "handle": "@spacex", 
+        "time": "2h", 
+        "image": "spacex.jpeg",
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345, "replies": 123, "retuits": 432, "disliked": false, "dislikes": 20
+        "liked": true, 
+        "likes": 2345, 
+        "replies": 123, 
+        "retuits": 432, 
+        "disliked": false, 
+        "dislikes": 20
     },
     {
         "_id": "234", "topic": "Traffic", "username": "The Boring Company",
