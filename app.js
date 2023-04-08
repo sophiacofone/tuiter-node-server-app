dfa7a4a357ee6a8eb9620e4f8d556d2459ad2250
sophiacofone <sophiacofone@gmail.com>
2023-04-08 18:26:01 -0400

    db

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-import HelloController from "./controllers/hello-controller.js"
-import UserController from "./controllers/users/users-controller.js"
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
@@ -14,3 +16,5 @@ UserController(app);
 TuitsController(app);
 
 app.listen(process.env.PORT || 4000);
+
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
